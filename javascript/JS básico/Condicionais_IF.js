8fa10fa54c3b879d903cac62e5f73b5e132f730e
Matheus Mello <matheus.rm.09@hotmail.com>
2021-07-27 11:53:04 -0300

    update

---
+++ javascript/JS básico/Condicionais_IF.js
@@ -0,0 +1,16 @@
+var idade = 16
+
+if (idade >= 35) {
+    console.log('Pode')
+    console.log('Qual o seu pedido?')
+}
+else if(idade >= 18 && idade <= 70) {
+    console.log('Pode')
+    console.log('Mostre a identidade')
+} 
+ 
+else if(idade < 18) {
+    console.log('Não pode')
+    console.log('Volte futuramente')
+}
+
