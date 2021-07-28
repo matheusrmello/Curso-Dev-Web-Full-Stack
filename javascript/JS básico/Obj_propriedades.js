ce38e065d7426f10189020f7c6257025ff0440ad
Matheus Mello <matheus.rm.09@hotmail.com>
2021-07-28 14:04:36 -0300

    up

--- javascript/JS básico/Obj_propriedades.js
+++ javascript/JS básico/Obj_propriedades.js
@@ -1,9 +1,9 @@
 var aluno = { nome: 'Matheus', notas: [5.7, 8.0] };
 
-var nomedeUmaPropriedade = 'sobrenome'
+var nomedeUmaPropriedade = 'sobrenome';
 
-aluno.matricula = 12344
+aluno.matricula = 12344;
 
-aluno[nomedeUmaPropriedade] = 'Mello'
+aluno[nomedeUmaPropriedade] = 'Mello';
 
-console.log(aluno)
+console.log(aluno);
