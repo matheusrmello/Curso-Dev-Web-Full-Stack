79c0e4efd2a70d1179afa7a7c7ef8bc600366adf
Matheus Mello <matheus.rm.09@hotmail.com>
2021-07-29 23:00:23 -0300

    .

--- javascript/JS básico/Obj_metodo.js
+++ javascript/JS básico/Obj_metodo.js
@@ -1,10 +1,12 @@
+function calcMedia(n1, n2){
+  return (this.nota[0] + this.nota[1]) / 2
+}
+
 var aluno = {
   nome: 'Matheus',
   nota: [10, 5],
 
-  media: function (n1, n2) {
-    return (n1 + n2) / 2;
-  },
+  media: calcMedia
 };
 
 
@@ -12,15 +14,13 @@ var aluno1 = {
     nome: 'Mariana',
     nota: [7, 6],
   
-    media: function (n1, n2) {
-      return (n1 + n2) / 2;
-    },
-  };
+    media: calcMedia
+  }
+
 
 console.log(aluno.nome);
-console.log(aluno.media(aluno.nota[0], aluno.nota[1]));
+console.log(aluno.media());
 
 
 console.log(aluno1.nome);
-console.log(aluno1.media(aluno1.nota[0], aluno1.nota[1]));
-
+console.log(aluno1.media());
